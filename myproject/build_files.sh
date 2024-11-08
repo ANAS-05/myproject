d572429e15538648a8ad771b49048af919644829
SYED ABDUL BARR ANAS <anassyed.dev@gmail.com>
2024-11-08 21:31:18 +0530

    Commit

--- myproject/build_files.sh
+++ myproject/build_files.sh
@@ -2,8 +2,10 @@
 
 # build_files.sh
 
+echo "Installling"
 # Install the required Python packages
 pip install -r requirements.txt
 
+echo "static files........."
 # Collect static files without user input
 python3.9 manage.py collectstatic --noinput
